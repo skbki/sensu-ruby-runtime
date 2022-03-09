8dc1062b48e59255b98045cdbc375e245e863405
Michał Skibicki <michal@skibicki.net>
2022-03-09 18:13:02 +0100

    buildx arm64

--- build_scripts/build_and_test_platform.sh
+++ build_scripts/build_and_test_platform.sh
@@ -22,7 +22,7 @@ else
   if [[ "$(docker images -q ${asset_image} 2> /dev/null)" == "" ]]; then
     echo "Docker image not found...we can build"
     echo "Building Docker Image: sensu-ruby-runtime:${ruby_version}-${platform}"
-    docker build --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t ${asset_image} -f Dockerfile.${platform} .
+    docker buildx --platform "linux/arm64" --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t ${asset_image} -f Dockerfile.${platform} .
     echo "Making Asset: /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_$(uname -m).tar.gz"
     docker run -v "$PWD/dist:/dist" ${asset_image} cp /assets/${asset_filename} /dist/
   #    #rm $PWD/test/*
@@ -37,7 +37,7 @@ fi
 test_arr=($test_platforms)
 for test_platform in "${test_arr[@]}"; do
   echo "Test: ${test_platform}"
-  docker run -e platform -e test_platform=${test_platform} -e asset_filename=${asset_filename} -v "$PWD/scripts/:/scripts" -v "$PWD/dist:/dist" ${test_platform} /scripts/test.sh
+  docker run --platform "linux/arm64" -e platform -e test_platform=${test_platform} -e asset_filename=${asset_filename} -v "$PWD/scripts/:/scripts" -v "$PWD/dist:/dist" ${test_platform} /scripts/test.sh
   retval=$?
   if [ $retval -ne 0 ]; then
     echo "!!! Error testing ${asset_filename} on ${test_platform}"
